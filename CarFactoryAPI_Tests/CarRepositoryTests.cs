0307e2484b4786ce81d1b459dfebb1bcc61295a4
RahmaRashedy <160316777+RahmaRashedy@users.noreply.github.com>
2024-05-16 12:04:31 +0300

    Add project files.

---
+++ CarFactoryAPI_Tests/CarRepositoryTests.cs
@@ -0,0 +1,118 @@
+﻿using CarAPI.Entities;
+using CarFactoryAPI.Entities;
+using CarFactoryAPI.Repositories_DAL;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Moq.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Sdk;
+
+
+namespace CarFactoryAPI_Tests
+{
+    public class CarRepositoryTests
+    {
+        // Create mock of dependencies
+        Mock<FactoryContext> contextMock;
+
+        // Use fake object as dependency
+        CarRepository carRepository;
+
+        List<Car> cars;
+        public CarRepositoryTests()
+        {
+            // Create Mock of Dependencies
+            contextMock = new();
+
+            // use fake object as dependency
+            carRepository = new(contextMock.Object);
+
+            cars = new List<Car>() {
+                new Car() { Id = 1},
+                new Car() { Id = 2},
+                new Car() { Id = 3}
+            };
+
+        }
+
+        [Fact]
+        public void GetAllCars_CarsList()
+        {
+            // Arrange
+           
+            // setup called Dbsets
+            contextMock.Setup(o => o.Cars).ReturnsDbSet(cars);
+
+            // Act
+            var result = carRepository.GetAllCars();
+
+            // Assert
+            Assert.Equal(cars, result);
+        }
+
+        [Fact]
+        public void AddCar_CarToAdd_True()
+        {
+            // Arrange
+
+            var car = new Car { Id = 100 };
+
+            // setup called Dbsets
+
+            contextMock.Setup(o => o.Cars.Add(car));
+            contextMock.Setup(o => o.SaveChanges());
+
+            // Act
+            var result = carRepository.AddCar(car);
+
+            // Assert
+            Assert.True(result);
+
+        }
+
+
+        [Fact]
+        public void AssignToOwner_CarNotNull_True()
+        {
+            // Arrange
+            Owner owner = new Owner() { Id = 100 };
+
+            // setup called Dbsets
+            contextMock.Setup(o => o.Cars).ReturnsDbSet(cars);
+
+            // Act
+            Car result = carRepository.GetCarById(1);
+            var result2 = carRepository.AssignToOwner(1, 100);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result2);
+
+        }
+
+        [Fact]
+        public void AssignToOwner_CarIsNull_False()
+        {
+
+            // Arrange
+            Owner owner = new Owner { Id = 10 };
+            contextMock.Setup(o => o.Cars).ReturnsDbSet(cars);
+            // Act
+            Car? result = carRepository.GetCarById(100);
+            var result2 = carRepository.AssignToOwner(100, 10);
+
+            // Assert
+            Assert.Null(result);
+            Assert.False(result2);
+        }
+
+
+    }
+}
+
+
---
+++ CarFactoryAPI_Tests/OwnersServiceTests.cs
@@ -0,0 +1,158 @@
+﻿using CarAPI.Entities;
+using CarAPI.Models;
+using CarAPI.Payment;
+using CarAPI.Repositories_DAL;
+using CarAPI.Services_BLL;
+using CarFactoryAPI.Entities;
+using CarFactoryAPI.Repositories_DAL;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace CarFactoryAPI_Tests
+{
+    public class OwnersServiceTests : IDisposable
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+        // Create Mock Of Dependencies
+        Mock<ICarsRepository> carRepoMock;
+        Mock<IOwnersRepository> OwnerRepoMock;
+        Mock<ICashService> cashMock;
+
+        // use fake object as a dependency
+        OwnersService ownersService;
+
+        public OwnersServiceTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+            // test setup
+            testOutputHelper.WriteLine("Test setup");
+            // Create Mock Of Dependencies
+            carRepoMock = new();
+            OwnerRepoMock = new();
+            cashMock = new();
+
+            // use fake object as a dependency
+            ownersService = new OwnersService(
+                carRepoMock.Object, OwnerRepoMock.Object, cashMock.Object);
+        }
+        public void Dispose() 
+        {
+            // test clean up
+            testOutputHelper.WriteLine("test clean up");
+        } 
+
+        // -- Owner already has a car -- //
+        [Fact]
+        [Trait("Author", "Aya")]
+        [Trait("Priorty", "3")]
+        public void BuyCar_OwenerHasCar_AlreadyHasCar()
+        {
+            testOutputHelper.WriteLine("Test 01");
+
+            //Build the mock data
+            Car car = new Car() { Id = 10, Price = 1000 };
+            Owner owner = new Owner { Car = new Car() };
+
+            //Setup called method
+            carRepoMock.Setup(o => o.GetCarById(10)).Returns(car);
+            OwnerRepoMock.Setup(o => o.GetOwnerById(2)).Returns(owner);
+            BuyCarInput carInput = new() { CarId = 10, OwnerId = 2, Amount = 1000 };
+
+            // Act 
+            string result = ownersService.BuyCar(carInput);
+
+            //Assert
+            Assert.Contains("have car", result);
+        }
+
+        // -- Owner doesn't have sufficient funds -- //
+        [Fact]
+        [Trait("Author", "Ali")]
+        [Trait("Priority", "4")]
+        public void BuyCar_InsufficientFunds_InsufficientFunds()
+        {
+            testOutputHelper.WriteLine("Test 02");
+            //Arrange
+
+            // Build the mock data
+            Car car = new Car() { Id = 10, Price = 1000 };
+            Owner owner = new Owner() { Id = 3};
+
+            // Setup called methods
+            carRepoMock.Setup(o => o.GetCarById(10)).Returns(car);
+            OwnerRepoMock.Setup(o => o.GetOwnerById(3)).Returns(owner);
+
+            BuyCarInput carInput = new() { CarId = 10, OwnerId = 3, Amount = 600 }; 
+
+            // Act
+            string result = ownersService.BuyCar(carInput);
+
+            // Assert
+            Assert.Contains("Insufficient", result);
+        }
+
+        // -- Failed to assign a car to the owner -- //
+        [Fact]
+        [Trait("Author", "Emad")]
+        [Trait("Priority", "5")]
+        public void BuyCar_AssignToOwnerFailure_SomethingWrong()
+        {
+            testOutputHelper.WriteLine("Test 03");
+
+            // Arrange
+
+            //Build the mock Data
+            Car car = new Car() { Id = 10, Price = 1000 };
+            Owner owner = new Owner() { Id = 4};
+
+            // Setup called methods
+            carRepoMock.Setup(o => o.GetCarById(10)).Returns(car);
+            OwnerRepoMock.Setup(o => o.GetOwnerById(4)).Returns(owner);
+         
+            BuyCarInput carInput = new() { CarId = 10, OwnerId = 4, Amount = 1000 };
+
+            // Act
+            string result = ownersService.BuyCar(carInput);
+
+            // Assert
+            Assert.Contains("wrong", result);
+        }
+
+        // -- Assign a car to the owner successfully -- //
+        [Fact]
+        [Trait("Author", "Sara")]
+        [Trait("Priority", "5")]
+        public void BuyCar_AssignToOwnerSuccessfully_Successfull()
+        {
+            testOutputHelper.WriteLine("Test 04");
+
+            // Arrange
+
+            //Build the mock Data
+            Car car = new Car() { Id = 10, Price = 1000 };
+            Owner owner = new Owner() { Id = 5, Name = "Sara" };
+
+            // Setup called methods
+            carRepoMock.Setup(o => o.GetCarById(10)).Returns(car);
+            OwnerRepoMock.Setup(o => o.GetOwnerById(5)).Returns(owner);
+            cashMock.Setup(o => o.Pay(1000)).Returns("Amount: 1000 is paid through Cash");
+            carRepoMock.Setup(r => r.AssignToOwner(It.IsAny<int>(), It.IsAny<int>())).Returns(true);
+
+            BuyCarInput carInput = new() { CarId = 10, OwnerId = 5, Amount = 1000 };
+
+            // Act
+            string result = ownersService.BuyCar(carInput);
+
+            // Assert
+            Assert.Contains("Successful", result);
+            
+        }
+
+
+    }
+}
